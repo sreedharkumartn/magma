0116e96be55cbd0ff9a8d3df143fe8e6b3b46dd0
Kaleemullah Mohammed <kaleemullah.mohammed@wavelabs.ai>
2021-09-07 14:22:27 +0530

    feat(amf): Support for default QoS
    Signed-off-by: Kaleemullah Mohammed <kaleemullah.mohammed@wavelabs.ai>

--- lte/gateway/c/core/oai/tasks/amf/amf_smf_send.cpp
+++ lte/gateway/c/core/oai/tasks/amf/amf_smf_send.cpp
@@ -18,6 +18,8 @@ extern "C" {
 #include "log.h"
 #include "conversions.h"
 #include "3gpp_38.401.h"
+#include "s6a_messages_types.h"
+
 #ifdef __cplusplus
 }
 #endif
@@ -29,6 +31,7 @@ extern "C" {
 #include "SmfServiceClient.h"
 #include "M5GMobilityServiceClient.h"
 #include "amf_app_timer_management.h"
+#include "mme_api.h"
 
 using magma5g::AsyncM5GMobilityServiceClient;
 using magma5g::AsyncSmfServiceClient;
@@ -614,6 +617,53 @@ int amf_smf_handle_ip_address_response(
   }
 
   return rc;
+}
+
+int amf_send_n11_update_location_req(amf_ue_ngap_id_t ue_id) {
+  OAILOG_FUNC_IN(LOG_AMF_APP);
+  ue_m5gmm_context_s* ue_context_p     = NULL;
+  MessageDef* message_p                = NULL;
+  s6a_update_location_req_t* s6a_ulr_p = NULL;
+  int rc                               = RETURNok;
+
+  OAILOG_INFO(
+      LOG_AMF_APP, "Sending UPDATE LOCATION REQ to subscriberd, ue_id =%u\n",
+      ue_id);
+
+  ue_context_p = amf_ue_context_exists_amf_ue_ngap_id(ue_id);
+
+  if (ue_context_p) {
+    OAILOG_INFO(
+        LOG_AMF_APP, "IMSI HANDLED =%u\n", ue_context_p->amf_context.imsi64);
+  } else {
+    OAILOG_ERROR(LOG_AMF_APP, "ue context not found for the ue_id=%u\n", ue_id);
+    OAILOG_FUNC_RETURN(LOG_AMF_APP, rc);
+  }
+
+  message_p = itti_alloc_new_message(TASK_AMF_APP, S6A_UPDATE_LOCATION_REQ);
+  if (message_p == NULL) {
+    OAILOG_FUNC_RETURN(LOG_AMF_APP, RETURNerror);
+  }
+
+  s6a_ulr_p = &message_p->ittiMsg.s6a_update_location_req;
+  memset((void*) s6a_ulr_p, 0, sizeof(s6a_update_location_req_t));
+
+  IMSI64_TO_STRING(
+      ue_context_p->amf_context.imsi64, s6a_ulr_p->imsi, IMSI_LENGTH);
+
+  s6a_ulr_p->imsi_length    = strlen(s6a_ulr_p->imsi);
+  s6a_ulr_p->initial_attach = INITIAL_ATTACH;
+  plmn_t visited_plmn       = {0};
+  COPY_PLMN(visited_plmn, ue_context_p->amf_context.originating_tai.plmn);
+  memcpy(&s6a_ulr_p->visited_plmn, &visited_plmn, sizeof(plmn_t));
+  s6a_ulr_p->rat_type = RAT_UTRAN;
+
+  // Set regional_subscription flag
+  s6a_ulr_p->supportedfeatures.regional_subscription = true;
+
+  rc = AsyncSmfServiceClient::getInstance().n11_update_location_req(s6a_ulr_p);
+
+  OAILOG_FUNC_RETURN(LOG_AMF_APP, rc);
 }
 
 }  // namespace magma5g
